8194ca2fd2ae249eb0ab04a80333fad92b7c8161
David Lorite <david.lorite@sysdig.comm>
2020-06-29 14:09:19 +0200

    Fix workflow deployment

--- deployments/staging/deploy.sh
+++ deployments/staging/deploy.sh
@@ -2,5 +2,5 @@
 kubectl version --client
 kubectl -n staging get po
 kubectl -n staging delete po -l app=frontend
-kubectl apply -f deployments/master/frontend-deployment.yaml
+kubectl apply -f deployments/staging/frontend-deployment.yaml
 kubectl -n staging get po
