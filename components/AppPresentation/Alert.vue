d9027b0b1bf8809b39b76ec4d61037b568151ea2
David de Torres <david.detorres@sysdig.com>
2021-04-05 13:28:46 +0200

    Add preview of alerts code

--- components/AppPresentation/Alert.vue
+++ components/AppPresentation/Alert.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <markdown :header-level-start="2" :content="resource.description" />
+    <H2>Alert files</H2>
     <Download
       v-for="alert in kindPrometheus"
       :key="alert.kind"
@@ -10,15 +11,26 @@
     >
       Import into {{ alert.kind }}
     </Download>
+    <H2>Alert details</H2>
+    To install these alerts in Sysdig Monitor, consult the <a href="https://docs.sysdig.com/en/promql-alerts.html#UUID-7b0f1744-88a0-a4dc-ac88-0bd70008bd18_UUID-9d9d7ee9-2898-a9aa-ea9b-4ab2328f3bf0">following documenattion</a>.
+    <prism
+      v-for="config in kindPrometheus"
+      :key="`${config.name}`"
+      language="yaml"
+      :code="config.data"
+      :filename="`${config.name}`"
+    />
   </div>
 </template>
 
 <script>
+import Prism from '@/components/Prism'
 import Markdown from '@/components/Markdown'
 import Download from '@/components/Download'
 
 export default {
   components: {
+    Prism,
     Markdown,
     Download
   },
